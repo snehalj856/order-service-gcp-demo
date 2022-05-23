808fb040b852230ed539c035e310121d251c715a
unknown <hemantpawar275@gmail.com>
2022-05-23 10:24:52 +0530

    updated scripts

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 cd /home/ec2-user/server/target
-sudo java -jar order-service-0.0.1-SNAPSHOT.jar
+sudo java -jar order-service-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
